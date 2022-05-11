c94963747c070449eda46614aef5414040b59617
Argin Aslanian <arginaslaniandeveloper@gmail.com>
2022-05-10 17:41:33 -0700

    Finished creating Resume PDF using FPDF in PHP File

--- generate-pdf.php
+++ generate-pdf.php
@@ -17,4 +17,78 @@
     $desc = $_POST['desc'];
     $skills = $_POST['skills'];
 
+    // Output on PDF file
+
+    // BOLD
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40, 10, $name);
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Location: ' . $location);
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Phone Number: ' . $phone);
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Email Address: ' . $email);
+    $pdf->Ln();
+
+    // BOLD
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40, 10, 'Education');
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'College: ' . $college);
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Degree: ' . $education);
+    $pdf->Ln();
+
+    // Regular
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Field of Study: ' . $field);
+    $pdf->Ln();
+
+    // BOLD
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40, 10, 'Work Experience');
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Company: ' . $company);
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Position: ' . $position);
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Description: ' . $desc);
+    $pdf->Ln();
+
+    // BOLD
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40, 10, 'Skills');
+    $pdf->Ln();
+
+    // REGULAR
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0, 10, 'Skills: ' . $skills);
+    $pdf->Ln();
+
+    $pdf->Output();
+
 ?>
