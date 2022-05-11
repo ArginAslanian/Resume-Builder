b6690d6b15ebcad0f244162ec594b8735bc5ce28
Argin Aslanian <arginaslaniandeveloper@gmail.com>
2022-05-10 17:38:21 -0700

    Get all required  data to PHP file

---
+++ generate-pdf.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Get all the required $_POST Data
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $college = $_POST['college'];
+    $education = $_POST['education'];
+    $field = $_POST['field'];
+    $company = $_POST['company'];
+    $position = $_POST['position'];
+    $desc = $_POST['desc'];
+    $skills = $_POST['skills'];
+
+?>
